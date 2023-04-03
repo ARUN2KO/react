0fe3ee1815c329dd6fe2769402991765f4cb9eb2
ARUN2KO <83573436+ARUN2KO@users.noreply.github.com>
2023-04-03 23:34:39 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,31 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <link rel="stylesheet" href="css/styles.css"></link>
+      <header className="App-header">
+
+      </header>
+      <div class="footer">
+        <table width="50%" cellspacing="0">
+
+
+          <tr>
+            <td>GET THE <br/><br/> FUTURE YOU <br/><br/>WANT<br/><br/><br/><br/></td>
+              <td></td>
+              <td ><br/>SUPPORT <br/><br/>YAMMER <br/><br/> TRAININGS<br/><br/><br/><br/></td>
+                <td><br/>PRIVACY POLICY <br/><br/> ABOUT US <br/><br/>CONTACT US<br/><br/><br/><br/></td>
+                </tr>
+                </table>
+                </div>
+                </div>               
+
+                
+                
+
+                  );
+}
+
+                  export default App;
